96e4237c22119c98222f715d1a24a6ec7ce78f77
Devan Kavalchek <devankav7@gmail.com>
2023-03-10 13:50:57 -0500

    Updated

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from official.nlp import optimization  # to create AdamW optimizer
 
 import matplotlib.pyplot as plt
 
-def build_classifier_model(tf_preprocess, tf_encoder):
+def build_classifier_model(preprocess, encoder):
    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-   preprocessing_layer = hub.KerasLayer(tf_preprocess, name='preprocessing')
+   preprocessing_layer = hub.KerasLayer(preprocess, name='preprocessing')
    encoder_inputs = preprocessing_layer(text_input)
-   encoder = hub.KerasLayer(tf_encoder, trainable=True, name='BERT_encoder')
+   encoder = hub.KerasLayer(encoder, trainable=True, name='BERT_encoder')
    outputs = encoder(encoder_inputs)
    net = outputs['pooled_output']
    net = tf.keras.layers.Dropout(0.1)(net)
@@ -88,6 +88,27 @@ def main():
   print(f'Pooled Outputs Values:{bert_results["pooled_output"][0, :12]}')
   print(f'Sequence Outputs Shape:{bert_results["sequence_output"].shape}')
   print(f'Sequence Outputs Values:{bert_results["sequence_output"][0, :12]}')
+
+  classifier_model = build_classifier_model(preprocessor, encoder)
+  bert_raw_result = classifier_model(tf.constant(text_test))
+  print(tf.sigmoid(bert_raw_result))
+
+  tf.keras.utils.plot_model(classifier_model)
+  loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+  metrics = tf.metrics.BinaryAccuracy()
+
+  epochs = 5
+  steps_per_epoch = tf.data.experimental.cardinality(train_ds).numpy()
+  num_train_steps = steps_per_epoch * epochs
+  num_warmup_steps = int(0.1*num_train_steps)
+
+  init_lr = 3e-5
+  optimizer = optimization.create_optimizer(
+     init_lr=init_lr,
+     num_train_steps=num_train_steps,
+     num_warmup_steps=num_warmup_steps,
+     optimizer_type='adamw'
+  )
 
 
 if __name__ == '__main__':
